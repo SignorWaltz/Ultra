d5b76f7b04535a03be3e81718efda7b9fdccadd4
Jurgen Ametaj <92936950+SignorWaltz@users.noreply.github.com>
2024-09-09 17:49:10 +0200

    v2

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ let currentIndex = 0;
 
 function showSlide(index) {
     const slides = document.getElementsByClassName('carousel-image');
-    if (index >= slides.length) currentIndex = 0;
-    if (index < 0) currentIndex = slides.length - 1;
+    const totalSlides = slides.length;
 
-    for (let i = 0; i < slides.length; i++) {
+    if (index >= totalSlides) currentIndex = 0;
+    if (index < 0) currentIndex = totalSlides - 3;
+
+    for (let i = 0; i < totalSlides; i++) {
         slides[i].style.display = "none";
     }
 
-    slides[currentIndex].style.display = "block";
+    // Mostra tre immagini per volta
+    for (let i = currentIndex; i < currentIndex + 3 && i < totalSlides; i++) {
+        slides[i].style.display = "block";
+    }
 }
 
 function moveCarousel(n) {
